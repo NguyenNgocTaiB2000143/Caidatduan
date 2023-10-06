5568b608ff70adc30e649324630557609fd048e4
NguyenNgocTaiB2000143 <crissemgamming@gmail.com>
2023-10-06 11:43:42 +0700

    Cai dat cac handler truy xuat CSDL

---
+++ app/config/index.js
@@ -0,0 +1,11 @@
+const config = {
+    app: {
+      port: process.env.port || 3000,
+    },
+    db: {
+      uri: process.env.MONGODB_URI || "mongodb://localhost:27017"
+    }
+  };
+  
+  module.exports = config;
+  
